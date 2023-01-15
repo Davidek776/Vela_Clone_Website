22c9dc69a4347256904b0c551ae93878f4fdc691
Davidek776 <kocurdavid776@gmail.com>
2023-01-15 20:05:44 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function ($) {
+  var left = $(".left li");
+
+  left.on("click", function () {
+    //left.css({ "backgroundColor":"#fff" });
+     //$(this).css({ "backgroundColor":"rgba(0, 0, 0, 0.1)" });
+    
+    //$(this).animate({"color": "#bada55"}, 1500);
+    /*$(this).animate({
+      "backgroundColor": "#bada55",
+      "color": "yellow",
+      "width": "500"
+    }, 1000 );
+    console.log($(this));*/
+    //$(this).css({"backgroundColor": "black", "color": "white"});
+
+  });
+})(jQuery);
